bac5285f95e50facdba8be3149f0eb2e824bcc61
lector <lector_225@mail.ru>
2026-07-18 10:44:39 +0500

    feat: add Login page with PrimeVue components

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppLayout from '@/layouts/AppLayout.vue'
 import PageView from '@/views/PageView.vue'
 import MyJobsView from '@/views/MyJobsView.vue'
+import LoginView from '@/views/LoginView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -21,6 +22,12 @@ const router = createRouter({
           name: 'my-jobs',
           component: MyJobsView,
           meta: { title: 'my-jobs' },
+        },
+        {
+          path: 'login',
+          name: 'login',
+          component: LoginView,
+          meta: { title: 'login' },
         },
       ],
     },
